460f6e0ca2b6c15734eb20f89b0a01fdd65807f6
ZenRool <lorenzopapperini@hotmail.it>
2022-04-19 13:33:01 +0200

    Terzo snak sommo i primi 10 numeri e faccio la media, soluzione sia con for che con formula di Gauss

---
+++ jsnack3/js/script.js
@@ -0,0 +1,9 @@
+let sum = 0;
+for (let i = 1 ; i <= 10 ; i++) {
+    sum += i;
+}
+console.log(`la somma è ${sum} e la media è ${sum/10}`);
+
+// Gauss
+const sum2 = (10+1)*10/2;
+console.log(`(Gauss) la somma è ${sum2} e la media è ${sum2/10}`);
