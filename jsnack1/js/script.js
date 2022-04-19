fdecc562e6dbb0b2bc1a0194ebecdcb009026802
ZenRool <lorenzopapperini@hotmail.it>
2022-04-19 13:35:08 +0200

    Aggiunto : mancante nel operatore ternario del primo snack

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,3 +1,3 @@
 const firstWord = prompt("inserisci la prima parola");
 const secondWord = prompt("inserisci la seconda parola");
-console.log((firstWord.length == secondWord.length) ? "Le parole sono lunghe uguali" (firstWord.length < secondWord.length) ? firstWord + '\n' + secondWord : secondWord + '\n' + firstWord );
+console.log((firstWord.length == secondWord.length) ? "Le parole sono lunghe uguali" : (firstWord.length < secondWord.length) ? firstWord + '\n' + secondWord : secondWord + '\n' + firstWord );
